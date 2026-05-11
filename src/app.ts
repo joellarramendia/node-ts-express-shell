23872706f05a1ce2da777f2b04167f8e524f9296
joellarramendia <joellarramendia@gmail.com>
2026-05-11 17:14:36 -0300

    feat: integrate MongoDB with Mongoose and add authentication routes

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { envs } from './config/envs.js';
+import { MongoDatabase } from './data/index.js';
 import { AppRoutes } from './presentation/routes.js';
 import { Server } from './presentation/server.js';
 
@@ -8,7 +9,12 @@ import { Server } from './presentation/server.js';
 })();
 
 
-function main() {
+async function main() {
+
+  await MongoDatabase.connect({
+    dbName: envs.MONGO_DB_NAME,
+    mongoUrl: envs.MONGO_URL
+  })
 
   const server = new Server({
     port: envs.PORT,
--- src/config/envs.ts
+++ src/config/envs.ts
@@ -5,6 +5,8 @@ import env from 'env-var';
 export const envs = {
 
   PORT: env.get('PORT').required().asPortNumber(),
+  MONGO_URL: env.get('MONGO_URL').required().asString(),
+  MONGO_DB_NAME: env.get('MONGO_DB_NAME').required().asString(),
 
 }
 
