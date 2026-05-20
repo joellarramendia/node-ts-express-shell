d73bd11d0e99bfc0417ce41bf0fefba1a2da85f8
joellarramendia <joellarramendia@gmail.com>
2026-05-20 16:47:16 -0300

    feat: implement uploadMultipleFiles method to handle multiple file uploads with validation

--- src/presentation/services/file-upload.service.ts
+++ src/presentation/services/file-upload.service.ts
@@ -44,11 +44,15 @@ export class FileUploadService {
 
     }
 
-    public uploadMultiple(
-        file: any[],
+    public async uploadMultiple(
+        files: UploadedFile[],
         folder: string = 'uploads',
         validExtensions: string[] = ['png', 'jpg', 'jpeg', 'gif']
     ) {
+        const fileNames = await Promise.all(
+            files.map(file => this.uploadSingle(file, folder, validExtensions))
+        )
 
+        return fileNames
     }
 }
